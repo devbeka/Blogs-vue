a05178b1ff756eb3a8bf15624496e2e00b1754c8
xkkaaaa <cholponbaevb655@gmail.com>
2022-06-30 01:44:50 +0600

    test

--- src/composables/getPost.js
+++ src/composables/getPost.js
@@ -1,24 +1,26 @@
-import {ref} from 'vue'
-import {firestore} from '../firebase/config';
+import { ref } from 'vue'
+import { firestore } from '../firebase/config'
 const getPost = (id) => {
-    const post = ref(null)
-    const error = ref(null)
+  const post = ref(null)
+  const error = ref(null)
 
-    const fetchPost = async () => {
-        try {
-            const response = await firestore.collection('posts').doc(id).get()
+  const fetchPost = async () => {
+    try {
+      const response = await firestore
+        .collection('posts')
+        .doc(id)
+        .get()
 
-            if(!response.exists) {
-                throw Error('Нет данных в базе')
-            }
+      if (!response.exists) {
+        throw Error('Нет данных в базе')
+      }
 
-            post.value = {...response.data(), id}
-        }
-        catch(err) {
-            error.value = err.message
-        }
+      post.value = { ...response.data(), id }
+    } catch (err) {
+      error.value = err.message
     }
-    return {post, error, fetchPost}
+  }
+  return { post, error, fetchPost }
 }
 
 export default getPost
